565f1069f75832bb6d9705df7e172a82d8b7822d
birechsheilah456-cmd <birechsheilah456@gmail.com>
2026-06-30 09:37:26 +0300

    Added payroll & Attendance

--- app/(dashboardlayout)/layout.tsx
+++ app/(dashboardlayout)/layout.tsx
@@ -1,18 +1,20 @@
+import type { Metadata } from 'next';
 
-export default function RootLayout({
+export const metadata: Metadata = {
+  title: 'HR Connect',
+  description: 'Attendance System',
+};
+
+export default function DashboardLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-    <html
-      lang="en"
-      className={ `h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">
-        
+    <html lang="en" className="h-full antialiased">
+      <body className="h-full bg-gray-50 antialiased">
         {children}
-        </body>
+      </body>
     </html>
   );
 }
